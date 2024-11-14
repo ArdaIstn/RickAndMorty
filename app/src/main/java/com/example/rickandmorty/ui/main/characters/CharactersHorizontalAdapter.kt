424b47ef41bda79a8f4009eef83c4e6860d4ee20
Arda <arda_isitan55@hotmail.com>
2024-11-14 13:55:00 +0300

    Added ui layer.

---
+++ app/src/main/java/com/example/rickandmorty/ui/main/characters/CharactersHorizontalAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.rickandmorty.ui.main.characters
+
+class CharactersHorizontalAdapter {
+}
---
+++ app/src/main/java/com/example/rickandmorty/ui/main/charactersdetail/CharactersDetailViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.rickandmorty.ui.main.charactersdetail
+
+class CharactersDetailViewModel {
+}
