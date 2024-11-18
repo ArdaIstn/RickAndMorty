e71c032db5c1222cd0922695966531c53e8893e6
Arda <arda_isitan55@hotmail.com>
2024-11-18 14:08:29 +0300

    Added DataSource.

---
+++ app/src/main/java/com/example/rickandmorty/data/datasource/CharacterDataSource.kt
@@ -0,0 +1,21 @@
+package com.example.rickandmorty.data.datasource
+
+import com.example.rickandmorty.data.model.Character
+import com.example.rickandmorty.data.model.LocationResult
+import com.example.rickandmorty.data.retrofit.CharacterApi
+import retrofit2.Response
+
+class CharacterDataSource(private val characterApi: CharacterApi) {
+
+    suspend fun getResultList(): Response<LocationResult> = characterApi.getLocations()
+
+    // Karakterlerin detaylarını çekmek için
+    suspend fun getCharacterDetailsByIds(ids: String): Response<List<Character>> {
+        return characterApi.getMultipleCharacters(ids)
+    }
+
+    suspend fun getCharacterDetailsById(id: String): Response<Character> {
+        return characterApi.getCharacterDetailsById(id)
+
+    }
+}
