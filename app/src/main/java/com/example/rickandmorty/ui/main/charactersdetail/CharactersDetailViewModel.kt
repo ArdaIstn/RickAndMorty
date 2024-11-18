68ba349a8d6f0489e1887551bd2f98e500559956
Arda <arda_isitan55@hotmail.com>
2024-11-18 14:12:44 +0300

    Refactor CharactersDetailViewModel to use LiveData instead of MutableLiveData for character property and clean up setter function.

--- app/src/main/java/com/example/rickandmorty/ui/main/charactersdetail/CharactersDetailViewModel.kt
+++ app/src/main/java/com/example/rickandmorty/ui/main/charactersdetail/CharactersDetailViewModel.kt
@@ -1,4 +1,18 @@
 package com.example.rickandmorty.ui.main.charactersdetail
 
-class CharactersDetailViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.rickandmorty.data.model.Character
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+
+class CharactersDetailViewModel : ViewModel() {
+    private val _character = MutableLiveData<Character>()
+    val character: MutableLiveData<Character> get() = _character
+
+    fun setCharacter(character: Character) {
+        _character.value = character
+    }
+
+
 }
