2eebe7bca3fa2c31aebea9688f6a908d2226c1f0
Arda <arda_isitan55@hotmail.com>
2024-11-18 14:10:31 +0300

    Added Constants.

--- app/src/main/java/com/example/rickandmorty/common/Constants.kt
+++ app/src/main/java/com/example/rickandmorty/common/Constants.kt
@@ -3,4 +3,9 @@ package com.example.rickandmorty.common
 object Constants {
     const val PREFS_NAME = "app_prefs"
     const val FIRST_LAUNCH_KEY = "first_launch"
+    const val BASE_URL = "https://rickandmortyapi.com/api/"
+    const val LOCATIONS_ENDPOINT = "location"
+    const val CHARACTERS_ENDPOINT = "character/{ids}"
+    const val CHARACTER_ENDPOINT = "character/{id}"
+
 }
