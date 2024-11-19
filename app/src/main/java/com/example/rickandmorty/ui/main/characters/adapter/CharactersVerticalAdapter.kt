6880bd1679c0035a30a3400edc8816643564bd1c
Arda <arda_isitan55@hotmail.com>
2024-11-19 21:16:17 +0300

    Added icons for character status.

--- app/src/main/java/com/example/rickandmorty/ui/main/characters/adapter/CharactersVerticalAdapter.kt
+++ app/src/main/java/com/example/rickandmorty/ui/main/characters/adapter/CharactersVerticalAdapter.kt
@@ -9,8 +9,10 @@ import com.example.rickandmorty.data.model.Character
 import com.example.rickandmorty.databinding.RvColumItemBinding
 
 class CharactersVerticalAdapter(
-    private val characters: List<Character>, private val onCharacterSelected: (Character) -> Unit
-) : RecyclerView.Adapter<CharactersVerticalAdapter.CharacterViewHolder>() {
+    private val characters: List<Character>,
+    private val onCharacterSelected: (Character) -> Unit,
+
+    ) : RecyclerView.Adapter<CharactersVerticalAdapter.CharacterViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
         val binding = RvColumItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -28,15 +30,16 @@ class CharactersVerticalAdapter(
         fun bind(character: Character) {
             binding.tvCharacterName.text = character.name
             binding.ivCharacter.loadImage(character.image)
+            binding.tvDeadOrAlive.text = character.status
 
-
-            if (character.gender == "Female") {
-                binding.characterCard.setBackgroundResource(R.color.female)
-            } else {
-                binding.ivCharacter.setBackgroundResource(R.color.male)
+            when (character.status) {
+                "Alive" -> binding.ivDeadOrAlive.setBackgroundResource(R.drawable.ic_alive)
+                "Dead" -> binding.ivDeadOrAlive.setBackgroundResource(R.drawable.ic_dead)
+                "unknown" -> binding.ivDeadOrAlive.setBackgroundResource(R.drawable.ic_unknown)
             }
-            binding.root.setOnClickListener { onCharacterSelected(character) }
 
+
+            binding.root.setOnClickListener { onCharacterSelected(character) }
 
         }
     }
