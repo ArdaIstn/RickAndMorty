b2570806d5424662fafa399a142ebefcfe5a91b4
Arda <arda_isitan55@hotmail.com>
2024-12-03 13:14:23 +0300

    Added observers for UI state updates in Fragment

--- app/src/main/java/com/example/rickandmorty/ui/main/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/rickandmorty/ui/main/characters/CharactersFragment.kt
@@ -2,31 +2,39 @@ package com.example.rickandmorty.ui.main.characters
 
 import com.example.rickandmorty.ui.main.characters.adapter.CharactersVerticalAdapter
 import android.os.Bundle
-import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.rickandmorty.common.navigateTo
 import com.example.rickandmorty.data.model.Character
-import com.example.rickandmorty.data.model.Result
 import com.example.rickandmorty.databinding.FragmentCharactersBinding
 import com.example.rickandmorty.ui.main.characters.adapter.LocationsHorizontalAdapter
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class CharactersFragment : Fragment() {
+
     private lateinit var binding: FragmentCharactersBinding
     private val viewModel: CharactersViewModel by viewModels()
 
-    private var horizontalScrollState: Parcelable? = null
-    private var verticalScrollState: Parcelable? = null
-    private var selectedLocationPosition: Int = RecyclerView.NO_POSITION
-    private var locationsAdapter: LocationsHorizontalAdapter? = null
-    private var charactersAdapter: CharactersVerticalAdapter? = null
+    private val locationsAdapter: LocationsHorizontalAdapter by lazy {
+        LocationsHorizontalAdapter { location ->
+            viewModel.fetchCharactersByLocation(location.residents)
+        }
+    }
+
+    private val charactersAdapter: CharactersVerticalAdapter by lazy {
+        CharactersVerticalAdapter { character ->
+            navigateToCharacterDetail(character)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -37,96 +45,71 @@ class CharactersFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupRecyclerViews()
         setupObservers()
     }
 
-    override fun onPause() {
-        super.onPause()
-        saveRecyclerViewStates()
-    }
-
-    private fun setupObservers() {
-        observeLocations()
-        observeCharacters()
-
-        viewModel.isEmpty.observe(viewLifecycleOwner) { isEmpty ->
-            binding.isEmpty.visibility = if (isEmpty) View.VISIBLE else View.GONE
-        }
-    }
-
-    private fun observeLocations() {
-        viewModel.locations.observe(viewLifecycleOwner) { locations ->
-            setupLocationsAdapter(locations)
-
-            if (selectedLocationPosition == RecyclerView.NO_POSITION && locations.isNotEmpty()) {
-                selectFirstLocation(locations)
-            }
-        }
-    }
-
-    private fun observeCharacters() {
-        viewModel.characters.observe(viewLifecycleOwner) { characters ->
-            setupCharactersAdapter(characters)
-        }
-    }
-
-    private fun setupLocationsAdapter(locations: List<Result>) {
-        locationsAdapter = LocationsHorizontalAdapter(locations) { location ->
-            resetVerticalScrollState() // Yeni lokasyonda listeyi sıfırla
-            viewModel.fetchCharactersByLocation(location.residents)
-        }.apply {
-            selectedPosition =
-                if (selectedLocationPosition == RecyclerView.NO_POSITION) 0 else selectedLocationPosition
-        }
-
+    private fun setupRecyclerViews() {
         binding.horizontalRv.apply {
             adapter = locationsAdapter
             layoutManager =
                 LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-            restoreHorizontalScrollState()
         }
-    }
-
-    private fun setupCharactersAdapter(characters: List<Character>) {
-        charactersAdapter = CharactersVerticalAdapter(characters) { character ->
-            navigateToCharacterDetail(character)
-        }
-
         binding.verticalRv.apply {
             adapter = charactersAdapter
-            layoutManager = LinearLayoutManager(requireContext())
-            scrollToTopOrRestoreState()
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
         }
     }
 
-    private fun selectFirstLocation(locations: List<Result>) {
-        selectedLocationPosition = 0
-        locationsAdapter?.notifyItemChanged(selectedLocationPosition)
-        viewModel.fetchCharactersByLocation(locations[0].residents)
-    }
+    private fun setupObservers() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.uiState.collect { state ->
+                handleErrorState(state)
+                handleLocationsState(state)
+                handleCharactersState(state)
+                handleEmptyState(state)
 
-    private fun saveRecyclerViewStates() {
-        horizontalScrollState = binding.horizontalRv.layoutManager?.onSaveInstanceState()
-        verticalScrollState = binding.verticalRv.layoutManager?.onSaveInstanceState()
-        selectedLocationPosition = locationsAdapter?.selectedPosition ?: RecyclerView.NO_POSITION
-    }
+                if (locationsAdapter.selectedPosition == -1) {
+                    val firstLocationWithResidents = state.locations?.firstOrNull { location ->
+                        location.residents.isNotEmpty()
+                    }
 
-    private fun restoreHorizontalScrollState() {
-        horizontalScrollState?.let { state ->
-            binding.horizontalRv.layoutManager?.onRestoreInstanceState(state)
+                    firstLocationWithResidents?.let {
+                        locationsAdapter.selectedPosition =
+                            0 // İlk lokasyonu seçili olarak işaretle
+                        viewModel.fetchCharactersByLocation(it.residents) // İlk lokasyondaki karakterleri yükle
+                    }
+                }
+
+            }
         }
     }
 
-    private fun scrollToTopOrRestoreState() {
-        if (verticalScrollState == null) {
-            binding.verticalRv.scrollToPosition(0)
-        } else {
-            binding.verticalRv.layoutManager?.onRestoreInstanceState(verticalScrollState)
+
+    private fun handleEmptyState(state: CharactersViewModel.CharacterState) {
+        if (state.isEmpty) {
+            Snackbar.make(requireView(), "No Data", Snackbar.LENGTH_SHORT).show()
+            state.isEmpty = false
         }
     }
 
-    private fun resetVerticalScrollState() {
-        verticalScrollState = null // Lokasyon değişiminde scroll durumunu sıfırla
+    private fun handleErrorState(state: CharactersViewModel.CharacterState) {
+        state.failMessage?.let {
+            Toast.makeText(requireContext(), "Data Error!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun handleLocationsState(state: CharactersViewModel.CharacterState) {
+        state.locations?.let { locations ->
+            locationsAdapter.differ.submitList(locations)
+        }
+    }
+
+    private fun handleCharactersState(state: CharactersViewModel.CharacterState) {
+        state.characters?.let { characters ->
+            charactersAdapter.differ.submitList(characters)
+        }
     }
 
     private fun navigateToCharacterDetail(character: Character) {
@@ -134,3 +117,5 @@ class CharactersFragment : Fragment() {
         navigateTo(action)
     }
 }
+
+
