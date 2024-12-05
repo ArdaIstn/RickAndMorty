2da6d3482445e0711b34be086b58dc74e8ddf9b4
Arda <arda_isitan55@hotmail.com>
2024-12-05 20:15:48 +0300

    Added BottomNav.

--- app/src/main/java/com/example/rickandmorty/ui/main/main/MainActivity.kt
+++ app/src/main/java/com/example/rickandmorty/ui/main/main/MainActivity.kt
@@ -2,15 +2,30 @@ package com.example.rickandmorty.ui.main.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.rickandmorty.R
+import com.example.rickandmorty.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupBottomNav()
+    }
 
+    private fun setupBottomNav() {
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
 
     }
 }
